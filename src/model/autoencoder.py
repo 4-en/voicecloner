9c65099f6e9de74d4ae8c0a166a9ee1dac37a4fa
4-en <ben.schumacher0@gmail.com>
2024-04-03 20:59:06 +0200

    add some stuff

--- src/model/autoencoder.py
+++ src/model/autoencoder.py
@@ -5,6 +5,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class DownLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -22,11 +23,12 @@ class DownLayer(nn.Module):
 class UpLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
         super(UpLayer, self).__init__()
-        self.conv = nn.ConvTranspose1d(in_channels, out_channels, kernel_size, stride, padding)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
         self.batchnorm = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU()
     
     def forward(self, x):
+        x = F.interpolate(x, scale_factor=2)
         x = self.conv(x)
         x = self.batchnorm(x)
         x = self.relu(x)
@@ -81,12 +83,59 @@ class Decoder(nn.Module):
     
 # VAE
 class VAE(nn.Module):
-    def __init__(self, n_layers):
+    def __init__(self, n_layers, deterministic=False):
         super(VAE, self).__init__()
         self.encoder = Encoder(n_layers)
         self.decoder = Decoder(n_layers)
+        self.deterministic = deterministic
     
     def forward(self, x):
+        # encode
         x = self.encoder(x)
+
+        
+        mean, logvar = x.chunk(2, dim=1)
+        if not self.deterministic:
+            # sample from the latent space
+            std = torch.exp(0.5*logvar)
+            eps = torch.randn_like(std)
+            x = mean + eps*std
+        else:
+            # use mean as the latent space
+            x = mean
+
+        # decode
         x = self.decoder(x)
         return x
+    
+    def loss(self, x, target):
+        # encode
+        x = self.encoder(x)
+        mean, logvar = x.chunk(2, dim=1)
+        
+        # KL loss
+        kl_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+        
+        if not self.deterministic:
+            # sample from the latent space
+            std = torch.exp(0.5*logvar)
+            eps = torch.randn_like(std)
+            x = mean + eps*std
+        else:
+            # use mean as the latent space
+            x = mean
+
+        # decode
+        x = self.decoder(x)
+        
+        # reconstruction loss
+        recon_loss = nn.MSELoss()(x, target)
+        
+        return kl_loss + recon_loss
+    
+def test():
+    vae = VAE(3)
+    x = torch.randn(1, 1, 1000)
+    target = torch.randn(1, 1, 1000)
+    loss = vae.loss(x, target)
+    print(loss)
